76cd680982591c5837feca569fd24f6babe88ebb
CorMcD317 <mcdoucor000@warren.k12.in.us>
2025-03-12 10:04:55 -0400

    third commit

--- main.py
+++ main.py
@@ -106,15 +106,15 @@ class Game:
             """Fire a bullet"""
             # Restrict the number of bullets on screen at a time
             if len(self.bullet_group) < 2:
-                self.shoot_sound = pygame.mixer.Sound("assets/audio")
+                self.shoot_sound.play()
+                PlayerBullet(self.rect.centerx, self.rect.top, self.bullet_group)
 
-        # TODO: (3/11/2025) call self.shoot_sound's play method.
-        # TODO: (3/11/2025) create  PlayerBullet passing in self.rect.centerx, self.rect.top, self.bullet_group
+
+        # TODO: (3/11/2025) create PlayerBullet passing in self.rect.centerx, self.rect.top, self.bullet_group
 
         def reset(self):
             """Reset the players position"""
             self.rect.centerx = WINDOW_WIDTH // 2
-            # TODO: (3/11/2025) assign half of the window width using // to self.rect.centerx
 
     class Alien(pygame.sprite.Sprite):
         """A class to model an enemy alien"""
